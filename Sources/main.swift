b3d3042036388923d6b57fa14123e97a79d88c21
Walter Carli <walkersneps@gmail.com>
2016-10-28 17:53:02 +0200

    Update intestation

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,20 +1,9 @@
 //
 //  main.swift
-//  PerfectTemplate
+//  Nomina-Romanorum
 //
-//  Created by Kyle Jessup on 2015-11-05.
-//	Copyright (C) 2015 PerfectlySoft, Inc.
+//  Created by Walter Carli on 28/10/16.
 //
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Perfect.org open source project
-//
-// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
-// Licensed under Apache License v2.0
-//
-// See http://perfect.org/licensing.html for license information
-//
-//===----------------------------------------------------------------------===//
 //
 
 import PerfectLib
