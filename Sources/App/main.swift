7efa814d4bea921e320da7434dec8db3d444d908
Walter Carli <walkersneps@gmail.com>
2016-11-03 17:00:59 +0100

    Working program

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,13 +1,61 @@
+import Foundation
 import Vapor
 
-let drop = Droplet()
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+// MARK: -- NAMES
+let praenomina: [String] = ["Marcus",
+                        "Gaius",
+                        "Titus",
+                        "Publius",
+                        "Lucius",
+                        "Cneus"]
+
+let nomina: [String] = ["Iulius",
+                        "Cornelius",
+                        "Claudius",
+                        "Cassius",
+                        "Sempronius",
+                        "Domitius"]
+
+let cognomina: [String] = ["Coriolanus",
+                        "Brutus",
+                        "Caligula",
+                        "Cato",
+                        "Caesar"]
+
+
+// MARK: -- Functions
+func buildName (from praenomina: [String], and nomina: [String], and cognomina: [String]) -> String {
+  let prae: Int = Int(arc4random_uniform(UInt32(praenomina.count)))
+  let n: Int = Int(arc4random_uniform(UInt32(nomina.count)))
+  let co: Int = Int(arc4random_uniform(UInt32(cognomina.count)))
+
+  return (praenomina[prae] + " " + nomina[n] + " " + cognomina[co])
 }
 
-drop.resource("posts", PostController())
+// MARK: -- Properties
+let defaultHead: String = "<head><meta charset='utf-8'></head>"
 
+
+// MARK: -- Program
+//create Droplet object
+let drop = Droplet()
+
+// REGISTER Routes and handlers
+//default route -> name generation
+drop.get { req in
+  return buildName(from: praenomina, and: nomina, and: cognomina)
+}
+
+//Brusky's easter egg
+drop.get("/brusky") { req in
+  return "BRUSCHIIIIII!! \n PARACADUTE! PARACADUTE!"
+}
+
+//my easter egg
+drop.get("w") { req in
+  return "Walter è un fregno della madonna"
+}
+
+// Start the server
 drop.run()
