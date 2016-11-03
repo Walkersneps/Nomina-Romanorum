5f2d55230efb9e912c13a8cddca3fef0d4d72585
Walter Carli <walkersneps@gmail.com>
2016-11-03 17:34:16 +0100

    Linux compatibility

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,4 +1,8 @@
-import Foundation
+#if os(Linux)
+  import Glibc
+#else
+  import Darwin
+#endif
 import Vapor
 
 
@@ -26,9 +30,17 @@ let cognomina: [String] = ["Coriolanus",
 
 // MARK: -- Functions
 func buildName (from praenomina: [String], and nomina: [String], and cognomina: [String]) -> String {
-  let prae: Int = Int(arc4random_uniform(UInt32(praenomina.count)))
-  let n: Int = Int(arc4random_uniform(UInt32(nomina.count)))
-  let co: Int = Int(arc4random_uniform(UInt32(cognomina.count)))
+  #if os(Linux)
+    return Int(random() % (max + 1))
+    let prae: Int = Int(random() % (praenomina.count + 1))
+    let n: Int = Int(random() % (nomina.count + 1))
+    let co: Int = Int(random() % (cognomina.count + 1))
+  #else
+    let prae: Int = Int(arc4random_uniform(UInt32(praenomina.count)))
+    let n: Int = Int(arc4random_uniform(UInt32(nomina.count)))
+    let co: Int = Int(arc4random_uniform(UInt32(cognomina.count)))
+  #endif
+
 
   return (praenomina[prae] + " " + nomina[n] + " " + cognomina[co])
 }
