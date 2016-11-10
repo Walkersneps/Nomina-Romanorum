5c02f68a812d6501c3d6fe634034be50cf6c5cc7
Walter Carli <walkersneps@gmail.com>
2016-11-10 18:35:24 +0100

    Add logging

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -7,12 +7,19 @@ import Vapor
 
 
 // MARK: -- NAMES
+print("\n Praeparandum Nomina-Romanorum...")
+print("Docendo nomina...")
 let praenominaObj = Praenomina(); let praenomina = praenominaObj.list
 let nominaObj = Nomina(); let nomina = nominaObj.list
 let cognominaObj = Cognomina(); let cognomina = cognominaObj.list
+print("Praenomina sunt: \(praenomina)")
+print("Nomina sunt: \(nomina)")
+print("Cognomina sunt: \(cognomina)")
+print("Nomina docta sunt\n")
 
 
 // MARK: -- Functions
+print("Docendo artem operandi...")
 func buildName (from praenomina: [String], and nomina: [String], and cognomina: [String]) -> String {
   #if os(Linux)
     let prae: Int = Int(random() % (praenomina.count + 1))
@@ -24,16 +31,19 @@ func buildName (from praenomina: [String], and nomina: [String], and cognomina: 
     let co: Int = Int(arc4random_uniform(UInt32(cognomina.count)))
   #endif
 
-
   return (praenomina[prae] + " " + nomina[n] + " " + cognomina[co])
 }
+print("Ars operandi docta est\n")
+
 
 // MARK: -- Properties
 let defaultHead: String = "<head><meta charset='utf-8'></head>"
 
+print("Configuratione completa est!\n\n")
 
 // MARK: -- Program
 //create Droplet object
+print("Incipit executio!")
 let drop = Droplet()
 
 // REGISTER Routes and handlers
