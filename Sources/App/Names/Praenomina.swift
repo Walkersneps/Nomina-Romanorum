b22b97f7247717662503ce00e28d30f180b19f2e
Walter Carli <walkersneps@gmail.com>
2017-11-18 15:33:10 +0100

    Make name lists static

--- Sources/App/Names/Praenomina.swift
+++ Sources/App/Names/Praenomina.swift
@@ -6,26 +6,26 @@
 //
 
 public struct Praenomina {
-    let list: [String] = [
-        "Appius",
-        "Aulus",
-        "Decimus",
-        "Gaius",
-        "Gneus",
-        "Kaeso",
-        "Lucius",
-        "Mamercus",
-        "Manius",
-        "Marcus",
-        "Numerius",
-        "Publius",
-        "Quintus",
-        "Servius",
-        "Sextus",
-        "Spurius",
-        "Tiberius",
-        "Titus",
-        "Vibius"
-    ]
+    static let list: [String] = [
+                                "Appius",
+                                "Aulus",
+                                "Decimus",
+                                "Gaius",
+                                "Gneus",
+                                "Kaeso",
+                                "Lucius",
+                                "Mamercus",
+                                "Manius",
+                                "Marcus",
+                                "Numerius",
+                                "Publius",
+                                "Quintus",
+                                "Servius",
+                                "Sextus",
+                                "Spurius",
+                                "Tiberius",
+                                "Titus",
+                                "Vibius"
+                                ]
 
 }
