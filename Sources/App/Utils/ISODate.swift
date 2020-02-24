7ab4fe242d634512a95d0282a5b49b55ed917336
Walter Carli <walkersneps@gmail.com>
2020-02-25 00:16:51 +0100

    Add dateutils

---
+++ Sources/App/Utils/ISODate.swift
@@ -0,0 +1,10 @@
+extension Date {
+  static func ISOString (from date: Date) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+
+    return dateFormatter.string(from: date).appending("Z")
+  }
+
+}
