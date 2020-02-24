f6e2004fb5d9e06e2ab74ca7f73d373600cca748
Walter Carli <walkersneps@gmail.com>
2020-02-24 21:24:39 +0100

    Add Nomina

---
+++ Sources/App/Names/Praenomina.swift
@@ -0,0 +1,30 @@
+//
+//  Praenomina.swift
+//  App
+//
+//  Created by Walter Carli on 15/11/17.
+//
+public struct Praenomina {
+    static let list: [String] = [
+                                "Appius",
+                                "Aulus",
+                                "Decimus",
+                                "Gaius",
+                                "Gneus",
+                                "Kaeso",
+                                "Lucius",
+                                "Mamercus",
+                                "Manius",
+                                "Marcus",
+                                "Numerius",
+                                "Publius",
+                                "Quintus",
+                                "Servius",
+                                "Sextus",
+                                "Spurius",
+                                "Tiberius",
+                                "Titus",
+                                "Vibius"
+                                ]
+
+}
