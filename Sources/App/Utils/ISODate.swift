ff7cbe83e8d4ab9c64971e19c6212c5a4d707759
Walter Carli <walkersneps@gmail.com>
2020-02-28 15:17:16 +0100

    Update ISODate.swift

--- Sources/App/Utils/ISODate.swift
+++ Sources/App/Utils/ISODate.swift
@@ -1,1 +1,14 @@
-import Foundationextension Date {  static func ISOString (from date: Date) -> String {    let dateFormatter = DateFormatter()    dateFormatter.timeZone = TimeZone(abbreviation: "UTC")    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"    return dateFormatter.string(from: date).appending("Z")  }}
+import Foundation
+
+extension Date {
+
+  static func ISOString (from date: Date) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+
+    return dateFormatter.string(from: date).appending("Z")
+  }
+
+
+}
